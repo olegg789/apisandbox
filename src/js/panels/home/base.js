bcb8b706b1ece1cd309546db3d64ea6ec1b33f62
Aleksandr Kuznetsov <sahakuz03@gmail.com>
2021-10-24 15:28:13 +0500

    вот

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -16,9 +16,21 @@ import {
 } from '@vkontakte/vkui'
 import {Icon16Done, Icon28Settings} from '@vkontakte/icons';
 
-
-
 class HomePanelBase extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            section: null 
+        };
+
+        /*
+            создаем объект state
+            в нём создаем section
+        */
+
+        this.onChange = this.onChange.bind(this);
+    }
 
     openSnackbar() {
         this.props.openPopout(
@@ -33,21 +45,42 @@ class HomePanelBase extends React.Component {
         );
     }
 
+    onChange(e) {
+        const { value } = e.currentTarget;
+        this.setState({ section: value }) // меняем значение section на value, которое находится в option
+    }
+
     render() {
         const {id, setPage} = this.props;
+        const {section} = this.state; //из this.state берём значение section, чтобы дальше просто писать "section", а не "this.state.section"
 
         return (
             <Panel id={id}>
-                <PanelHeader
-                    left={<PanelHeaderButton onClick={() => setPage('home', 'placeholder')}><Icon28Settings/></PanelHeaderButton>}>Выполнить</PanelHeader>
+                <PanelHeader left={<PanelHeaderButton onClick={() => setPage('home', 'placeholder')}><Icon28Settings/></PanelHeaderButton>}>Выполнить</PanelHeader>
                 <Group>
                     <FormItem top="Выберите раздел">
-                        <NativeSelect placeholder="Не выбран">
+                        <NativeSelect 
+                            placeholder="Не выбран"
+                            onChange={this.onChange} //запускается, когда меняется значение элемента
+                        >
                             {method.map((el, index) => {
                                 return <option value={index}>{el}</option>
                             })}
                         </NativeSelect>
                     </FormItem>
+
+                    {section !== null && //если section не равен нач. значению, то отображаем второй NativeSelect, для выбора метода
+                        <FormItem top="Выберите метод">
+                            <NativeSelect 
+                                placeholder="Не выбран"
+                                //onClick={this.onChange}
+                            >
+                                {method.map((el, index) => {
+                                    return <option value={index}>{el}</option>
+                                })}
+                            </NativeSelect>
+                        </FormItem>
+                    }
                 </Group>
             </Panel>
         );
