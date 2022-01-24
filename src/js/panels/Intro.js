fa64a29b982f8be2d168bed326334a6bc01c0ce6
olegg789 <olegchikelev789789@gmail.com>
2022-01-24 19:13:05 +0300

    new design for intro

--- src/js/panels/Intro.js
+++ src/js/panels/Intro.js
@@ -8,13 +8,23 @@ import {
     Panel,
     Div,
     Button,
-    Group,
     Gallery,
-    Title,
+    FixedLayout,
+    Placeholder,
+    Group,
 } from "@vkontakte/vkui";
 import bridge from "@vkontakte/vk-bridge";
+import {Icon20HelpOutline, Icon28CarOutline, Icon56GestureOutline} from "@vkontakte/icons";
 
 class Intro extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            slideIndex: 0
+        };
+
+    }
 
     setUserSeenIntro() {
         let {panelsHistory} = this.props;
@@ -26,44 +36,77 @@ class Intro extends React.Component {
 
     render() {
         const {id} = this.props;
+        const {slideIndex} = this.state;
 
         return (
             <Panel id={id}>
-                <Group>
-                    <Div>
-                        <Gallery
-                            slideWidth="100%"
-                            align="center"
-                            style={{ height: '100%' }}
-                            showArrows
-                        >
-                            <Div>
-                                <Title level="1" weight="semibold" style={{ marginBottom: 16 }}>
-                                    ПРИВЕТ
-                                </Title>
-                            </Div>
-                            <Div>
-                                <Title level="1" weight="semibold" style={{ marginBottom: 16 }}>
-                                    ЭТО АПИ СЕНДБОКС
-                                </Title>
-                            </Div>
-                            <Div>
-                                <Title level="1" weight="semibold" style={{ marginBottom: 16 }}>
-                                    ПОЕХАЛИ
-                                </Title>
-                                <Div>
-                                    <Button
-                                        stretched
-                                        size='l'
-                                        onClick={() => this.setUserSeenIntro()}
+                <FixedLayout vertical='top'>
+                    <Group>
+                        <Div>
+                            <Gallery
+                                align="center"
+                                style={{ height: '100%', justifyContent: "center"}}
+                                slideIndex={slideIndex}
+                            >
+                                <div>
+                                    <Placeholder
+                                        icon={<Icon56GestureOutline />}
+                                        header="Привет, это сервис VK API/VK Bridge Sandbox"
+                                        action={<Button onClick={() => this.setState({slideIndex: this.state.slideIndex + 1})} size="m">Дальше</Button>}
                                     >
-                                        СТАРТ
-                                    </Button>
-                                </Div>
-                            </Div>
-                        </Gallery>
-                    </Div>
-                </Group>
+                                        Здесь ты можешь легко и быстро протестировать любой метод из API или Bridge.<br/><br/>
+                                    </Placeholder>
+                                </div>
+                                <div>
+                                    <Placeholder
+                                        icon={<Icon20HelpOutline width={56} height={56}/>}
+                                        header="Что и как"
+                                        action={
+                                            <>
+                                                <Button className='btn_onboarding' onClick={() => this.setState({slideIndex: this.state.slideIndex - 1})} size="m">Назад</Button>
+                                                <Button className='btn_onboarding' onClick={() => this.setState({slideIndex: this.state.slideIndex + 1})} size="m">Дальше</Button>
+                                            </>
+                                        }
+
+                                    >
+                                        Все просто:<br/> 1) Выбираешь нужный тебе метод API или Bridge на соответствующей панели<br/> 2) Вводишь необходимые параметры и получаешь ответ
+                                    </Placeholder>
+                                </div>
+                                <div>
+                                    <Placeholder
+                                        icon={<Icon20HelpOutline width={56} height={56}/>}
+                                        header="А что если у меня нет токена?"
+                                        action={
+                                            <>
+                                                <Button className='btn_onboarding' onClick={() => this.setState({slideIndex: this.state.slideIndex - 1})} size="m">Назад</Button>
+                                                <Button className='btn_onboarding' onClick={() => this.setState({slideIndex: this.state.slideIndex + 1})} size="m">Дальше</Button>
+                                            </>
+                                        }
+                                    >
+                                        Ты можешь получить его у нас. Перейди на панель "Настройки" и нажми кнопку "Получить токен".<br/>
+                                        К сожалению, с таким токеном получится вызвать не все методы API(
+
+                                    </Placeholder>
+                                </div>
+
+                                <div>
+                                    <Placeholder
+                                        icon={<Icon28CarOutline width={56} height={56}/>}
+                                        header='Всё понятно?'
+                                        action={
+                                            <>
+                                                <Button className='btn_onboarding' onClick={() => this.setState({slideIndex: this.state.slideIndex - 1})} size="l">Назад</Button>
+                                                <Button size='l' onClick={() => this.setUserSeenIntro()}>Поехали!</Button>
+                                            </>
+                                        }
+                                    >
+
+                                    </Placeholder>
+                                </div>
+                            </Gallery>
+                        </Div>
+                    </Group>
+                </FixedLayout>
             </Panel>
         )
     }
